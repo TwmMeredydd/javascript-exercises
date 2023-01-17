985a63a483a616584f99a95558b86608bc316a5a
TwmMeredydd <twmmeredydd@gmail.com>
2023-01-17 15:40:38 +0000

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
+    if (n1<0 || n2<0 || !Number.isInteger(n1) || !Number.isInteger(n2)) {
+        return "ERROR";
+    }
 
+    numbers = [n1, n2].sort((a, b) => a-b);
+    sum = 0;
+    for (let i = numbers[0]; i < numbers[1]+1; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
