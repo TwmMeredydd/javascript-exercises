ea4f3341dc0052be2337737996f652a1d7bbb7dd
TwmMeredydd <twmmeredydd@gmail.com>
2023-01-17 23:27:34 +0000

    Complete 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, next) => {
+        return calculateAge(oldest.yearOfBirth, oldest.yearOfDeath) 
+            < calculateAge(next.yearOfBirth, next.yearOfDeath) 
+            ? next : oldest;
+    })
 };
+
+function calculateAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
