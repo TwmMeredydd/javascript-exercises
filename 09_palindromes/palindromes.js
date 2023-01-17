7fa38809bd5b10e7ccc0b0c3a93241c7818dc844
TwmMeredydd <twmmeredydd@gmail.com>
2023-01-17 22:50:45 +0000

    Complete 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let bare = string.toLowerCase().replace(/[,.!\s]/g, "");
+    return bare.split("").reduceRight((acc, next) => acc + next, "") == bare;
 };
 
 // Do not edit below this line
