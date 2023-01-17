270f75e95e6ad4379977503a5b0e12e3d235c2c8
TwmMeredydd <twmmeredydd@gmail.com>
2023-01-17 22:37:37 +0000

    Complete 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	return a.reduce((acc, n) => acc + n, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  return a.reduce((acc, n) => acc * n, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  total = 1;
+	for (; n > 1; n--) total *= n;
+  return total;
 };
 
 // Do not edit below this line
