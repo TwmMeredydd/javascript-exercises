202c59e82b650ee1115623bc67a639d3f20ba434
TwmMeredydd <twmmeredydd@gmail.com>
2023-01-17 15:54:12 +0000

    Complete 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  return oneDecimal((f - 32) * (5 / 9));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  return oneDecimal(c * (9/5) + 32);
 };
+
+function oneDecimal(n) {
+  return Math.round(n*10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
